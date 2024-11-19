0427b8522d02985e80efdce8a0f60347383c6f67
cmij.rey <cmij.rey@gmail.com>
2024-11-19 09:27:28 +0000

    modified:   main.py modified:   users.db

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ database.create_database("users")
 database.create_user('Joe', 25, "users")
 database.create_user('Gerald', 30, "users")
 database.create_user('Alice', 19, "users")
+
+# READ
+database.get_user('Alice')
+#database.print_database()
+
+#UPDATE
+database.update_user('Joe', 40)
+database.get_user('Joe')
+
+#DELETE
+database.delete_user('Alice')
+database.print_database()
