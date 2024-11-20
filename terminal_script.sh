b1c896a8faae38ae127721b26b455026368dca40
cmij.rey <cmij.rey@gmail.com>
2024-11-20 06:35:49 +0000

    modified:   misc-class-notes.txt

---
+++ terminal_script.sh
@@ -0,0 +1,10 @@
+!/bin/sh
+# This is a shell script!
+# You can write terminal commands here and they will
+# all execute when you 'run' this script.
+# also it took me forever to realize I did not have 
+# to close '' or ()
+echo Start shell script
+la
+pwd
+echo End shell script
